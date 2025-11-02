dc1e972088b396d8d6da5eba8086eadbdd106609
press2025 <preston.machanga@aol.com>
2025-11-02 18:08:54 +0300

    updated-homepage

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Get the current year dynamically
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Get the last modified date of the document
+document.getElementById("lastModified").textContent = `Last modified: ${document.lastModified}`;
